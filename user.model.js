64bcaedb4c14bad6b3ec68a1b36fb4bdeaef4763
tawfik hegazy <58559677+tawfik-hegazy@users.noreply.github.com>
2025-08-16 21:52:14 +0300

    Add files via upload

---
+++ user.model.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: [true, 'User name is required'] },
+  email: {
+    type: String,
+    required: [true, 'Email is required'],
+    unique: true,
+    validate: [validator.isEmail, 'Enter a valid email'],
+  },
+  password: {
+    type: String,
+    required: [true, 'Password is required'],
+    minlength: [8, 'Minimum password length is 8 characters'],
+  },
+  photo: { type: String, default: 'profile.webp' },
+ staredCourse: [String]
+});
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
